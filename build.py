7c07642a9568e8434274c08767f7d2c8d5dda5af
Shardul Pakhale <47375180+omssp@users.noreply.github.com>
2023-05-20 13:14:33 +0530

    build using py 3.7

--- build.py
+++ build.py
@@ -1,7 +1,47 @@
+import re
+import ast
 import json
 import shutil
+import slugify
 
-output_dir = "dist/retrogamezone/"
+OUTPUT_DIR = "dist/retrogamezone/"
 
-shutil.rmtree(output_dir, ignore_errors=True)
-shutil.copytree('src/', output_dir, dirs_exist_ok=True)
+shutil.rmtree(OUTPUT_DIR, ignore_errors=True)
+shutil.copytree('src/', OUTPUT_DIR)
+
+slugs = []
+with open(f"{OUTPUT_DIR}lib/gamelist.js", "r+") as list_js:
+    data = list_js.read()
+    obj = data[data.find('['): data.rfind(';')]
+    obj = re.sub(r"\/\/\s.*", '', obj, re.MULTILINE)
+    games = ast.literal_eval(obj)
+
+    for game in games:
+        slug = slugify.slugify(game[0], replacements=[['&', 'and']])
+        game.append(slug)
+        slug = f"{OUTPUT_DIR}{slug}.html"
+        slugs.append(slug)
+        if slugs.count(slug) > 1:
+            print(
+                f"WARNING!!! Duplicate found in gamelist.js : {slug} : {game}")
+            del game
+            slugs.pop()
+
+    # print(len(set(slugs)))
+    # print(games)
+
+    list_js.seek(0)
+    list_js.truncate()
+    list_js.write(f"var gameNameArray = {json.dumps(games, indent=4)};")
+
+with open(f"{OUTPUT_DIR}index.html", "r+") as home:
+    content = home.read()
+    content = content.replace(
+        "game.html?${$(this).attr('gameid')}", "/${$(this).attr('gameid')}/")
+
+    home.seek(0)
+    home.truncate()
+    home.write(content)
+
+for slug in slugs:
+    shutil.copy("src/game.html", slug)
