085e2b6f859b1baef86e6fd72d341617e5500acc
Shardul Pakhale <47375180+omssp@users.noreply.github.com>
2023-05-20 13:41:43 +0530

    Update build.py

--- build.py
+++ build.py
@@ -37,11 +37,30 @@ with open(f"{OUTPUT_DIR}lib/gamelist.js", "r+") as list_js:
 with open(f"{OUTPUT_DIR}index.html", "r+") as home:
     content = home.read()
     content = content.replace(
-        "game.html?${$(this).attr('gameid')}", "/${$(this).attr('gameid')}/")
+        'gameid="${gameInfo[3]}"', 'gameid="${gameInfo[4]}" game-slug="${gameInfo[3]}"')
+    content = content.replace(
+        "game.html?${$(this).attr('gameid')}", "${$(this).attr('game-slug')}")
+
+    home.seek(0)
+    home.truncate()
+    home.write(content)
+
+
+with open(f"{OUTPUT_DIR}game.html", "r+") as home:
+    content = home.read()
+    content = content.replace(
+        'var para = 1;', 'var para = "slug";')
+    content = content.replace("window.onload = function () {",
+                              """window.onload = function () {
+
+            if (para == "slug") {
+                let slug = document.location.pathname.replace("/", "");
+                para = gameNameArray.findIndex(o => o[3] == slug);
+            }""")
 
     home.seek(0)
     home.truncate()
     home.write(content)
 
 for slug in slugs:
-    shutil.copy("src/game.html", slug)
+    shutil.copy(f"{OUTPUT_DIR}game.html", slug)
