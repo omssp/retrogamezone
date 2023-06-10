912176990ace7d67dbe06d7723eb612a85f9149d
Shardul Pakhale <47375180+omssp@users.noreply.github.com>
2023-06-10 18:26:48 +0530

    Update build.py

--- build.py
+++ build.py
@@ -350,6 +350,7 @@ for index, slug in enumerate(slugs):
         ['//cdn.jsdelivr.net/gh/omssp/nesscraper/nesicon.png', games[index][2]],
         ['./manifest.json', f'./{games[index][3]}.json'],
         ['src="index2.html"','src="index2"'],
+        ['src="index.html"','src="/"'],
     ]
     slug_contents = '%s' % contents
     for pair in replacements:
