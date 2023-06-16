0874eef49df6475c7c55bb2011e2bdc6c2b58d4e
Shardul Pakhale <47375180+omssp@users.noreply.github.com>
2023-06-16 22:50:50 +0530

    refactored worker

--- src/worker.js
+++ src/worker.js
@@ -19,9 +19,20 @@ saveHandler.storage_cfkv = class storage_cfkv {
         this.env = env
     }
 
-    saveKV = (key, data) => this.env.RETROGZ.put(key, data)
+    saveKV = async (key, data, subkey) => {
+        let store = data
+        if (subkey) {
+            store = await this.getKV(key, '')
+            store = store ? store : {}
+            store[subkey] = data
+        }
+        return await this.env.RETROGZ.put(key, JSON.stringify(store))
+    }
 
-    getKV = key => this.env.RETROGZ.get(key)
+    getKV = async (key, subkey) => {
+        const data = JSON.parse(await this.env.RETROGZ.get(key))
+        return subkey ? data[subkey] ? data[subkey] : null : data
+    }
 }
 
 saveHandler.storage_upstash = class storage_upstash {
@@ -42,14 +53,17 @@ saveHandler.storage_upstash = class storage_upstash {
 
     
     executeCommand = async (command) => {
-        const resp = await fetch(this.upstash_url, this.requestOptions(command))
+        const resp = await fetch(this.upstash_url, this.requestOptions(command.filter(o => o)))
         const json = await resp.json()
-        return json.error ? JSON.stringify(json.error) : json.result
+        return json.error ? JSON.stringify([json.error]) : json.result
     }
 
-    saveKV = (key, data) => this.executeCommand(["SET", key, data])
+    saveKV = (key, data, subkey) => this.executeCommand(["JSON.SET", key, subkey ? `$.${subkey}` : '$', JSON.stringify(data)])
 
-    getKV = key => this.executeCommand(["GET", key])
+    getKV = async (key, subkey) => {
+        const data = JSON.parse(await this.executeCommand(["JSON.GET", key, subkey ? `$.${subkey}` : '$']))
+        return data ? data.length? data[0] : null : null
+    }
 }
 
 export default {
@@ -87,35 +101,20 @@ export default {
 
             // if (config.supported_keys.indexOf(KEY_NAME) < 0) throw "wrong key" 
 
-            console.log(PATH, SLUG, method)
+            // console.log(PATH, SLUG, method, KEY_NAME)
 
             if (method == "GET") {
-
-                let data = JSON.parse(await storage_handler.getKV(SLUG))
-
-                if (data && KEY_NAME) {
-                    data = data[KEY_NAME] ? data[KEY_NAME] : null
-                }
-
+                let data = await storage_handler.getKV(SLUG, KEY_NAME)
                 return RETURN_JSON(data)
             } else if (method == "PUT") {
 
-                const payload = await request.json()
+                const PAYLOAD = await request.json()
 
-                let data = payload
+                await storage_handler.saveKV(SLUG, PAYLOAD, KEY_NAME)
 
-                if (KEY_NAME) {
-                    data = JSON.parse(await storage_handler.getKV(SLUG))
-                    data = data ? data : {}
-                    data[KEY_NAME] = payload
-                }
-
-                await storage_handler.saveKV(SLUG, JSON.stringify(data))
-
-                if (PRINT == 'silent') {
+                if (PRINT == 'silent')
                     return new Response("", { headers: DEFAULT_HEADERS, status: 204 })
-                }
-                return RETURN_JSON(payload)
+                return RETURN_JSON(PAYLOAD)
             }
 
             return RETURN_JSON(null)
